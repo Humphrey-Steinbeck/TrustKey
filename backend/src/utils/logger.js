2823c39dc0121bdfcb29a2e138f5580827870c42
PaulLoden <khafijur790@gmail.com>
2023-10-15 10:30:00 +0800

    feat: add winston logger utility for structured logging and error tracking

---
+++ backend/src/utils/logger.js
@@ -0,0 +1,55 @@
+// TrustKey Logger Utility
+
+const winston = require('winston');
+const path = require('path');
+
+// Define log levels
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+// Define log colors
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'blue',
+};
+
+winston.addColors(colors);
+
+// Define log format
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`
+  )
+);
+
+// Define log transports
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: path.join(__dirname, '../../logs/error.log'),
+    level: 'error',
+  }),
+  new winston.transports.File({
+    filename: path.join(__dirname, '../../logs/combined.log'),
+  }),
+];
+
+// Create logger instance
+const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || 'debug',
+  levels,
+  format,
+  transports,
+});
+
+module.exports = logger;
