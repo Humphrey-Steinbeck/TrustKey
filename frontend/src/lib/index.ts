1c0c25bf9992ddb96b1db6006c0579be9605deb7
Humphrey-Steinbeck <melclaudine0@gmail.com>
2024-02-23 10:59:47 +0800

    feat: add library index file for centralized library exports

---
+++ frontend/src/lib/index.ts
@@ -0,0 +1,9 @@
+// TrustKey Library Index
+
+// Export all library modules
+export * from './api';
+export * from './web3';
+
+// Re-export commonly used utilities
+export { apiClient, api } from './api';
+export { web3Service } from './web3';
