12ff20dad939939325112b3cd7651b7b1b0f592f
Humphrey-Steinbeck <melclaudine0@gmail.com>
2024-03-15 10:17:54 +0800

    feat: add components index file for centralized component exports

---
+++ frontend/src/components/index.ts
@@ -0,0 +1,20 @@
+// TrustKey Components Index
+
+// Export all components for centralized imports
+export { default as Button } from './Button';
+export { default as Input } from './Input';
+export { default as Card, CardHeader, CardContent, CardFooter } from './Card';
+export { default as Modal, ModalHeader, ModalContent, ModalFooter } from './Modal';
+export { default as Toast, ToastContainer } from './Toast';
+export { default as Loading, LoadingOverlay, LoadingButton } from './Loading';
+export { default as Header } from './Header';
+export { default as Footer } from './Footer';
+export { default as Layout } from './Layout';
+
+// Export component types
+export type { ButtonProps } from './Button';
+export type { InputProps } from './Input';
+export type { CardProps, CardHeaderProps, CardContentProps, CardFooterProps } from './Card';
+export type { ModalProps, ModalHeaderProps, ModalContentProps, ModalFooterProps } from './Modal';
+export type { ToastProps, ToastContainerProps } from './Toast';
+export type { LoadingProps, LoadingOverlayProps, LoadingButtonProps } from './Loading';
