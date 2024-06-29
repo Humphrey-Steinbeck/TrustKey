dc3202e62041c75f611849d8e7cb76c26fae81b8
Humphrey-Steinbeck <melclaudine0@gmail.com>
2024-06-29 20:10:27 +0800

    feat: add contexts index file for centralized context exports

---
+++ frontend/src/contexts/index.ts
@@ -0,0 +1,11 @@
+// TrustKey Contexts Index
+
+// Export all contexts for centralized imports
+export { Web3Provider, useWeb3 } from './Web3Context';
+export { ToastProvider, useToast } from './ToastContext';
+export { ThemeProvider, useTheme } from './ThemeContext';
+
+// Export context types
+export type { Web3ContextType } from './Web3Context';
+export type { ToastContextType } from './ToastContext';
+export type { ThemeContextType } from './ThemeContext';
